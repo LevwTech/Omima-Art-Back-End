68312f2ca8ba4fb55db259578ac29fd3e7683d17
levvw <abdelraahmanmostafa@gmail.com>
2022-02-12 19:21:34 +0200

    finished main backend

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const app = express();
 const productRouter = require("./routers/product");
 const exhibitionRouter = require("./routers/exhibition");
+const mailRouter = require("./routers/mail");
 app.use(cors());
 app.use(express.static("uploads")); // serving images folder publicly
 app.use(express.json());
 app.use(productRouter);
 app.use(exhibitionRouter);
+app.use(mailRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`server started on port ${process.env.PORT}`);
