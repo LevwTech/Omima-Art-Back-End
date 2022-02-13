e9efd47380596b734c95e40ad72641ce3937ff45
levvw <abdelraahmanmostafa@gmail.com>
2022-02-13 21:49:59 +0200

    trying to solve images not showing issue

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const exhibitionRouter = require("./routers/exhibition");
 
 app.use(cors());
 process.env.PWD = process.cwd();
-// app.use(express.static("uploads")); // serving images folder publicly
-app.use(express.static(path.join(process.env.PWD, "public")));
+app.use(express.static("uploads")); // serving images folder publicly
+app.use(express.static(path.join(process.env.PWD, "uploads")));
 app.use(express.json());
 app.use(productRouter);
 app.use(exhibitionRouter);
