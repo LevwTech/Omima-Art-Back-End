7e178d9ff703b31124d7043578b9a5c2d7c7779a
levvw <abdelraahmanmostafa@gmail.com>
2022-03-13 22:01:49 +0200

    added payment route

--- routers/product.js
+++ routers/product.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Product = require("../models/product");
 const multer = require("multer");
 const uploadFile = require("../utils/s3Upload");
+const axios = require("axios");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/");
@@ -158,5 +159,60 @@ router.get("/prevorders", async (req, res) => {
 // payment route:  first api takes key (acc unique) gives token to be used in 2nd api
 // 2nd api gives id to be used in 3rd api as order_id along with also token
 // 3rd api takes integration_id (acc unique) and gives token to be used in iframe
+router.post("/payment", async (req, res) => {
+  const data1 = await axios.post("https://accept.paymob.com/api/auth/tokens", {
+    api_key: process.env.PAYMOB_API_KEY,
+  });
+  const token = data1.data.token;
+
+  const obj2 = {
+    auth_token: token,
+    delivery_needed: "false",
+    amount_cents: String(Number(req.body.items.price) * 15.75 * 100),
+    currency: "EGP",
+    items: [
+      {
+        name: req.body.items.title,
+        amount_cents: String(Number(req.body.items.price) * 15.75 * 100),
+        description: req.body.items.desc,
+        quantity: "1",
+      },
+    ],
+  };
+
+  const data2 = await axios.post(
+    "https://accept.paymob.com/api/ecommerce/orders",
+    obj2
+  );
+  const id = data2.data.id;
+  const obj3 = {
+    auth_token: token,
+    amount_cents: String(req.body.items.price * 15.75 * 100),
+    expiration: 3600,
+    order_id: id,
+    billing_data: {
+      apartment: "NA",
+      email: req.body.user.email,
+      floor: "NA",
+      first_name: req.body.user.name,
+      street: req.body.shipping.adress,
+      building: "NA",
+      phone_number: req.body.shipping.phone,
+      shipping_method: "NA",
+      postal_code: "NA",
+      city: req.body.shipping.city,
+      country: req.body.shipping.country,
+      last_name: req.body.user.sub,
+      state: "NA",
+    },
+    currency: "EGP",
+    integration_id: process.env.PAYMOB_INTEGRATION_ID,
+  };
+  const data3 = await axios.post(
+    "https://accept.paymob.com/api/acceptance/payment_keys",
+    obj3
+  );
+  res.send({ token: data3.data.token });
+});
 
 module.exports = router;
