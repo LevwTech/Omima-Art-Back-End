30827ff002295eaa3249f71b9665a97430df385e
levvw <abdelraahmanmostafa@gmail.com>
2022-09-09 10:38:59 +0200

    updated cors policy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 const productRouter = require("./routers/product");
 const exhibitionRouter = require("./routers/exhibition");
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://omima.art" }));
 process.env.PWD = process.cwd();
 app.use(express.static("uploads")); // serving images folder publicly
 app.use(express.static(path.join(process.env.PWD, "uploads")));
