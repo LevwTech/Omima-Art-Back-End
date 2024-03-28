309ae45c1f496da9f257dbbf64653d7917106f47
Abdelrahman Mostafa <Abdelraahmanmostafa@gmail.com>
2024-03-28 16:08:52 +0200

    fix: add parser size limit

--- index.js
+++ index.js
@@ -13,9 +13,8 @@ app.use(cors({ origin: "*" }));
 process.env.PWD = process.cwd();
 app.use(express.static("uploads")); // serving images folder publicly
 app.use(express.static(path.join(process.env.PWD, "uploads")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true, limit: '30mb' }));
-app.use(express.json({limit:'30mb'})); 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(productRouter);
 app.use(exhibitionRouter);
 app.use(paymentRouter);
